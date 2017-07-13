3c7dac55518858d97364ccf16db55e975d4a6dc2
Ajay Chenampara <ajay.chenampara@gmail.com>
2017-07-13 11:19:39 -0400

    update md5sum checker

--- files/md5_updater.sh
+++ files/md5_updater.sh
@@ -1,6 +1,5 @@
-for f in /home/ajay/Projects/misc/list_of_dicts/*.py; do
+for f in /var/lib/tftpboot/*.py; do
     cat $f | sed '/^#md5sum/d' > $f.md5 ; sed -i \
                                               "s/^#md5sum=.*/#md5sum=\"$(md5sum $f.md5 | sed 's/ .*//')\"/" $f
-    echo $f
 done
 
